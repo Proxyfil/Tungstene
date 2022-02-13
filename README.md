975dcd8e0bb4122921876f36e454e4f85e04b175
Proxyfil <piloupilou200@gmail.com>
2022-02-13 21:24:04 +0100

    feat : version 1.0

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Tungstene
-Python Script to get every users connected to a chat Twitch. Part of Chrono-Break Project
+Python Script to get every users connected to a chat Twitch. 
+Part of Chrono-Break Project
+
+## Requirements
+ - **pyyaml** python lib for reading config file
+
+## Usage
+### *config.yml* is the default configuration file you can edit it like so :
+- *requests* is the sub-part dedicated to requests params
+    - *streamers* contain the list of streamers logins you want to track
+    - *delay* define the time to wait between requests **(in ms)**
+    - *start* in seconds after epoch (Unix) to begin the scan
+    - *end* in seconds after epoch (Unix) to end the scan
---
+++ tungstene.py
@@ -0,0 +1,81 @@
+#Imports
+import requests
+import time
+import yaml
+import json
+
+#Variables Declarements
+config_file = "config.yml"
+
+#Functions Declarement
+def api_requests(channel):
+    r = requests.get('https://tmi.twitch.tv/group/user/'+channel+'/chatters')
+    return r.json()["chatters"]["viewers"]
+
+def verification(database,resp,channel):
+    if(channel in database.keys()):
+            for viewer in resp:
+                if(viewer not in database[channel]):
+                    database[channel].append(viewer)
+    else:
+        database[channel] = resp
+
+    return database
+
+def read(file):
+    data = open(file,"r",encoding="utf-8")
+    content = ""
+    for char in data:
+        content += char
+    return json.loads(content)
+
+def write(file,data):
+    database_file = open(file,"w",encoding="utf-8")
+    json.dump(data, database_file, indent = 4)
+    database_file.close()
+
+def display_init():
+    print(" ______                                 __                            \n/\__  _\                               /\ \__                         \n\/_/\ \/ __  __    ___      __     ____\ \ ,_\    __    ___      __   \n   \ \ \/\ \/\ \ /' _ `\  /'_ `\  /',__\\ \ \/  /'__`\/' _ `\  /'__`\ \n    \ \ \ \ \_\ \/\ \/\ \/\ \L\ \/\__, `\\ \ \_/\  __//\ \/\ \/\  __/ \n     \ \_\ \____/\ \_\ \_\ \____ \/\____/ \ \__\ \____\ \_\ \_\ \____\ \n      \/_/\/___/  \/_/\/_/\/___L\ \/___/   \/__/\/____/\/_/\/_/\/____/\n                            /\____/                                   \n                            \_/__/                                    \n")
+
+def main():
+    display_init()
+    print(f"[WARMUP] Starting every protocols...")
+    print(f"[WARMUP] Config file as {config_file}")
+
+    config_yml = open(config_file,"r",encoding="utf-8")
+    config = yaml.load(config_yml, Loader=yaml.loader.SafeLoader)
+
+    if(config["requests"]["start"]-time.time() < 0):
+        print("[ERROR] Start timestamp is in the past... You can't do this :/")
+        exit()
+    elif(config["requests"]["start"]-time.time() > config["requests"]["end"]-time.time()):
+        print("[ERROR] Start timestamp is after End Timestamp... You can't do this :/")
+        exit()
+
+    database = read("global_map.json")
+
+    display_temp = config["requests"]["start"]-time.time()
+    display_temp_global = time.gmtime(config["requests"]["start"])
+    print(f"[WARMUP] Starting in {display_temp} seconds (or {display_temp_global[3]+1}:{display_temp_global[4]}:{display_temp_global[5]} {display_temp_global[2]}/{display_temp_global[1]}/{display_temp_global[0]} UTC+1)")
+    time.sleep(config["requests"]["start"]-time.time())
+
+    display_temp = config["requests"]["end"]-time.time()
+    display_temp_global = time.gmtime(config["requests"]["end"])
+    print(f"[LOGS] Starting Protocols --- Ending in {display_temp} seconds (or {display_temp_global[3]+1}:{display_temp_global[4]}:{display_temp_global[5]} {display_temp_global[2]}/{display_temp_global[1]}/{display_temp_global[0]} UTC+1)")
+
+    while(time.time() < config["requests"]["end"]):
+        for channel in config["requests"]["streamers"]:
+            resp = api_requests(channel)
+            database = verification(database,resp,channel)
+        write("global_map.json",database)
+
+        print(f"[LOGS] Everything went right. Waiting for next scan")
+        time.sleep(config["requests"]["delay"])
+
+    print(f"[EXIT] Program ended due to time limit")
+
+    
+
+    
+if __name__ == "__main__":
+    main()
