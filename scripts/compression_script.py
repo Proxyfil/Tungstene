e8eefcafa0cbbe05b28bca1af182bd69e93232d3
Proxyfil <piloupilou200@gmail.com>
2022-02-23 21:12:48 +0100

    feat : indev-compression

--- scripts/compression_script.py
+++ scripts/compression_script.py
@@ -1,4 +1,6 @@
 import json
+import os
+import datetime
 
 def compression(input_file,compress_type):
     if(compress_type == "simple"):
@@ -7,28 +9,42 @@ def compression(input_file,compress_type):
         return("[ERROR] An error occured")
 
 def compression_t1(input_file):
-    viewers = {}
-    compressed_file = {}
-    compressed_viewers = {}
-    compressed_streamers = {}
+    file_path = "./data/"+datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")+"/"
 
-    file = json.loads(input_file)
-    for streamer in file.keys():
-        for viewer in file[streamer]:
-            if viewer in viewers.keys():
-                viewers[viewer].append(streamer)
+    os.mkdir(file_path)
+    viewers_list = {}
+    label_list = {}
+    id = 0
+
+    name_to_id = {}
+    viewers = {"nodes": {}, "links": {}} #Viewers to Alt
+    alt = {"nodes": {}, "links": {}} #Alt to Viewers
+    mother = {"nodes": [], "links": []} #Alt and Streamers
+
+    file = json.loads(input_file) #Load file
+    for streamer in file.keys(): #For each streamer in the keys of the file
+        for viewer in file[streamer]: #For each viewer of this streamer
+            if(viewer not in name_to_id.keys()): #If viewer is not registered
+                name_to_id[viewer] = hex(id)[2:] #Hex number ID
+                id += 1
+            
+            if(viewer in viewers_list.keys()): #If viewer registered with his streamers
+                viewers_list[viewer].append(streamer) #Add this streamer
             else:
-                viewers[viewer] = [streamer]
+                viewers_list[viewer] = [streamer] #Set this streamer
 
-    for viewer in viewers.keys():
-        label = " | "
-        for streamer in viewers[viewer]:
-            label += streamer+" | "
-        for streamer in viewers[viewer]:
-            if(streamer in compressed_streamers.keys()):
-                compressed_streamers[streamer].append(label)
-            else:
-                compressed_streamers[streamer] = [label]
+    for key in viewers_list.keys(): #For viewer in keys
+        if " | ".join(viewers_list[key]) not in label_list.keys(): #If streamers label of this viewer not already registered
+            label_list[" | ".join(viewers_list[key])] = [key] #Set label with viewer in it
+            alt["nodes"][" | ".join(viewers_list[key])] = [{"id":name_to_id[key],"type":"viewer"}] #Set viewer as child-node of this alt
+        else:
+            label_list[" | ".join(viewers_list[key])].append(key) #Add viewer to label_list with label as id
+            alt["nodes"][" | ".join(viewers_list[key])].append({"id":name_to_id[key],"type":"viewer"}) #Add viewer as child-node of this alt
 
-        if(label in compressed_viewers.keys()):
-            compressed_viewers[label].append(viewer)
+    #Build mother
+    #Write File
+
+with open("./data/global_map.json","r") as f:
+    compression(f.read(),"simple")
+
+    
