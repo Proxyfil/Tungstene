32e5051678633160741bac8e5e2446e5dd3a037d
Proxyfil <piloupilou200@gmail.com>
2022-03-01 19:42:20 +0100

    feat : new node syntax

--- scripts/compression_script.py
+++ scripts/compression_script.py
@@ -23,6 +23,7 @@ def compression_t1(input_file):
 
     file = json.loads(input_file) #Load file
     for streamer in file.keys(): #For each streamer in the keys of the file
+        mother["nodes"].append({"id": streamer, "name": streamer, "type": "streamer", "value": 50})
         for viewer in file[streamer]: #For each viewer of this streamer
             if(viewer not in name_to_id.keys()): #If viewer is not registered
                 name_to_id[viewer] = hex(id)[2:] #Hex number ID
@@ -41,10 +42,38 @@ def compression_t1(input_file):
             label_list[" | ".join(viewers_list[key])].append(key) #Add viewer to label_list with label as id
             alt["nodes"][" | ".join(viewers_list[key])].append({"id":name_to_id[key],"type":"viewer"}) #Add viewer as child-node of this alt
 
-    #Build mother
+    maxi = 0
+    for key in label_list.keys():
+        mother["nodes"].append({"id": key, "name": key, "type": "alt", "value": len(label_list[key]), "fx":0, "fy":0, "fz":0})
+        maxi = max(maxi, len(label_list[key]))
+        for streamer_login in key.split(" | "):
+            mother["links"].append({"source": key, "target": streamer_login})
+    
+    seuil = 0
+    while(len(mother["nodes"]) > 1000):
+        seuil+=0.1
+        print(f"Formating mother list to seuil {seuil}")
+        for node in mother["nodes"]:
+            if(node["value"]/maxi*50 < seuil):
+                mother["nodes"].remove(node)
+                if(node["id"] in label_list.keys()):
+                    label_list.pop(node["id"])
+            else:
+                node["value"] = node["value"]/maxi*50
+
+    print(f"Formating mother links to seuil")
+    for link in mother["links"]:
+        if(link["source"] not in label_list.keys()):
+            mother["links"].remove(link)
+
+    with open(file_path+'mother.json', 'w') as f:
+        json.dump(mother, f)
+    
+    print(len(label_list.keys()))
+    print(len(file.keys()))
     #Write File
 
-with open("./data/global_map.json","r") as f:
+with open("./data_outgithub/art_gl_global.json","r") as f:
     compression(f.read(),"simple")
 
     
